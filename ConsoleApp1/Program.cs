a744cf394983dc9763a657a093ec2d24db82247d
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2024-01-09 16:26:10 +0200

    created new repository

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿class Airplane
+{
+    public string StartCity;
+    public string FinishCity;
+
+    public string StartDate;
+    public string FinishDate;
+
+
+    public GetTotalTime()
+    {
+
+    }
+
+    public IsArrivingToday()
+    {
+
+    }
+}
+
+
+
+
+class MyDate
+{
+    public string Year;
+    public string Month;
+    public string Day;
+    public string Hours;
+    public string Minutes;
+}
+
+class Product
+{
+    public string Name;
+    public string Price;
+    public string Cost;
+    public int Quantity;
+    public string Producer;
+    public string Weight;
+
+    public GetPriceInUAH() 
+    {
+    
+    } 
+    public GetTotalPriceInUAH() 
+    {
+    
+    } 
+    public GetTotalWeight() 
+    {
+    
+    }
+
+}
+
+class Currency
+{
+    public string Name;
+    public string ExRate;
+}
