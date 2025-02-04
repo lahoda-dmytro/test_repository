13bd053995978baa11f56aa721e19be46e58f727
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-02-04 17:05:25 +0200

    fixed date format and some error

--- ConsoleApp1/Airplane.cs
+++ ConsoleApp1/Airplane.cs
@@ -8,93 +8,47 @@ namespace ConsoleApp1
 {
     public class Airplane
     {
-        protected string StartCity;
-        protected string FinishCity;
-        protected MyDate StartDate;
-        protected MyDate FinishDate;
-
-        public Airplane()
-        {
-            StartDate = new MyDate();
-            FinishDate = new MyDate();
-        }
+        private readonly string startCity;
+        private readonly string finishCity;
+        private readonly MyDate startDate;
+        private readonly MyDate finishDate;
 
         public Airplane(string startCity, string finishCity, MyDate startDate, MyDate finishDate)
         {
-            StartCity = startCity;
-            FinishCity = finishCity;
-            StartDate = new MyDate(startDate);
-            FinishDate = new MyDate(finishDate);
+            if (string.IsNullOrWhiteSpace(startCity))
+                throw new ArgumentException("Start city cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(finishCity))
+                throw new ArgumentException("Finish city cannot be empty.");
+
+            if (startDate == null || finishDate == null)
+                throw new ArgumentNullException("StartDate and FinishDate cannot be null.");
+
+            if (startDate.GetDateTime() > finishDate.GetDateTime())
+                throw new ArgumentException("Finish date cannot be earlier than start date.");
+
+            this.startCity = startCity;
+            this.finishCity = finishCity;
+            this.startDate = new MyDate(startDate);
+            this.finishDate = new MyDate(finishDate);
         }
 
-        public Airplane(Airplane otherAirplane)
-        {
-            StartCity = otherAirplane.StartCity;
-            FinishCity = otherAirplane.FinishCity;
-            StartDate = new MyDate(otherAirplane.StartDate);
-            FinishDate = new MyDate(otherAirplane.FinishDate);
-        }
-
-
-
-        public void SetStartCity(string startCity)
-        {
-            this.StartCity = startCity;
-        }
-
-
-        public void SetFinishCity(string finishCity)
-        {
-            this.FinishCity = finishCity;
-        }
-        public void SetStartDate(MyDate startDate)
-        {
-            this.StartDate = startDate;
-        }
-        public void SetFinishDate(MyDate finishDate)
-        {
-            this.FinishDate = finishDate;
-        }
-
-
-
-
-        public string GetStartCity()
-        {
-            return this.StartCity;
-
-        }
-
-        public string GetFinishCity()
-        {
-            return this.FinishCity;
-        }
-
-        public MyDate GetStartDate()
-        {
-            return this.StartDate;
-        }
-        public MyDate GetFinishDate()
-        {
-            return this.FinishDate;
-
-
-        }
-
+        public string StartCity => startCity;
+        public string FinishCity => finishCity;
+        public MyDate StartDate => startDate;
+        public MyDate FinishDate => finishDate;
 
         public int GetTotalTime()
         {
-            TimeSpan duration = FinishDate.GetDateTime().Subtract(StartDate.GetDateTime());
+            TimeSpan duration = finishDate.GetDateTime().Subtract(startDate.GetDateTime());
             return (int)duration.TotalMinutes;
         }
 
         public bool IsArrivingToday()
         {
-            bool isSameDay = StartDate.GetDay() == FinishDate.GetDay();
-            bool isSameMonth = StartDate.GetMonth() == FinishDate.GetMonth();
-            bool isSameYear = StartDate.GetYear() == FinishDate.GetYear();
-
-            return isSameDay && isSameMonth && isSameYear;
+            return startDate.Year == finishDate.Year &&
+                   startDate.Month == finishDate.Month &&
+                   startDate.Day == finishDate.Day;
         }
     }
 
--- ConsoleApp1/Product.cs
+++ ConsoleApp1/Product.cs
@@ -8,16 +8,14 @@ namespace ConsoleApp1
 {
     public class Product
     {
-        protected string Name;
-        protected decimal Price;
-        protected Currency Cost;
-        protected int Quantity;
-        protected string Producer;
-        protected double Weight;
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Currency Cost { get; set; }
+        public int Quantity { get; set; }
+        public string Producer { get; set; }
+        public double Weight { get; set; }
 
-        public Product()
-        {
-        }
+        public Product() { }
 
         public Product(string name, decimal price, Currency cost, int quantity, string producer, double weight)
         {
@@ -29,91 +27,10 @@ namespace ConsoleApp1
             Weight = weight;
         }
 
-        public Product(Product otherProduct) : this(otherProduct.Name, otherProduct.Price, otherProduct.Cost, otherProduct.Quantity, otherProduct.Producer, otherProduct.Weight)
-        {
-        }
+        public Product(Product otherProduct) : this(otherProduct.Name, otherProduct.Price, otherProduct.Cost, otherProduct.Quantity, otherProduct.Producer, otherProduct.Weight) { }
 
-
-
-        public string GetName()
-        {
-            return Name;
-        }
-
-        public decimal GetPrice()
-        {
-            return Price;
-        }
-
-        public Currency GetCost()
-        {
-            return Cost;
-        }
-
-        public int GetQuantity()
-        {
-            return Quantity;
-        }
-
-        public string GetProducer()
-        {
-            return Producer;
-        }
-
-        public double GetWeight()
-        {
-            return Weight;
-        }
-
-
-
-        public void SetName(string name)
-        {
-            Name = name;
-        }
-
-        public void SetPrice(decimal price)
-        {
-            Price = price;
-        }
-
-        public void SetCost(Currency cost)
-        {
-            Cost = new Currency(cost);
-        }
-
-
-        public void SetQuantity(int quantity)
-        {
-            Quantity = quantity;
-        }
-
-        public void SetProducer(string producer)
-        {
-            Producer = producer;
-        }
-
-        public void SetWeight(double weight)
-        {
-            Weight = weight;
-        }
-
-
-
-        public decimal GetPriceInUAH()
-        {
-            return Price * Cost.GetExRate();
-        }
-
-        public decimal GetTotalPriceInUAH()
-        {
-            return Price * Quantity * Cost.GetExRate();
-        }
-
-        public double GetTotalWeight()
-        {
-            return Weight * Quantity;
-        }
+        public decimal PriceInUAH => Price * Cost.GetExRate();
+        public decimal TotalPriceInUAH => Price * Quantity * Cost.GetExRate();
+        public double TotalWeight => Weight * Quantity;
     }
-
 }
